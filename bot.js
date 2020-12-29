28112f67e94b9f870e6eeaaa1503dad3ade0147e
TecDeveloper <67659592+TecDeveloper@users.noreply.github.com>
2020-12-30 00:11:51 +0300

    Bot.js

---
+++ bot.js
@@ -0,0 +1,16 @@
+const Discord = require('discord.js');
+const client = new Discord.Client();
+const seçenekler = require('./seçenekler.json');
+
+var prefix = seçenekler.prefix;
+var owner = seçenekler.owner;
+
+const log = message => {
+  console.log(`Bot bağlandı`);
+};
+
+if (message.content === 'Sa') {
+	message.channel.send('Aleyküm selam, hoşgeldin');
+}
+
+client.login(seçenekler.token);
